aa09bffe54bdedeefb71c47eb4afab54bd965d32
Amir Madankan <madankan.amir.2000@gmail.com>
2023-03-27 01:24:08 +0430

    code updated

--- Ch01_HW01.py
+++ Ch01_HW01.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from openpyxl import load_workbook, Workbook
 
-ws = load_workbook("files/x2.xlsx")["Sheet1"]
+ws = load_workbook("files/x3.xlsx")["Sheet1"]
 max_row = ws.max_row
 
 data = []
@@ -16,38 +16,56 @@ y = data[:, 1]
 dy = y[1:] - y[:-1]
 dx = x[1:] - x[:-1]
 
-ddy = dy[1:] - dy[:-1]
-ddx = dx[1:] - dx[:-1]
-
 yprime = y.copy()
 yprime[1:] = dy/dx
 yprime[0] = yprime[1]
 
+wb_save = Workbook()
+ws_save = wb_save.active
+
+i = 0
+for x_i in x:
+    ws_save[f"A{i+1}"] = x_i
+    ws_save[f"B{i+1}"] = yprime[i]
+    i += 1
+
+wb_save.save("files/3x2.xlsx")
+
+ws2 = load_workbook("files/3x2.xlsx")["Sheet"]
+max_row2 = ws2.max_row
+
+data2 = []
+for i in range(1, max_row2+1):
+    data2.append([ws2[f"A{i}"].value, ws2[f"B{i}"].value])
+
+data2 = np.array(data2)
+dx = data2[:, 0]
+dy = data2[:, 1]
+
+ddy = dy[1:] - dy[:-1]
+ddx = dx[1:] - dx[:-1]
+
 ydoubleprime = yprime.copy()
-ydoubleprime[2:] = ddy/ddx
-# ydoubleprime[0] = ydoubleprime[1]
+ydoubleprime[1:] = ddy/ddx
+ydoubleprime[0] = ydoubleprime[1]
 
-# wb_save = Workbook()
-# ws_save = wb_save.active
-# wb_save2 = Workbook()
-# wb_save2 = wb_save2.active
+wb_save2 = Workbook()
+ws_save2 = wb_save2.active
 
-# i = 0
-# for x_i in x:
-#     ws_save[f"A{i+1}"] = x_i
-#     ws_save[f"B{i+1}"] = yprime[i]
-#     wb_save2[f"A{i+1}"] = x_i
-#     wb_save2[f"B{i+1}"] = ydoubleprime[i]
-#     i += 1
+k = 0
+for dx_i in dx:
+    ws_save2[f"A{k+1}"] = dx_i
+    ws_save2[f"B{k+1}"] = ydoubleprime[k]
+    k += 1
 
-# wb_save.save("files/dexp.xlsx")
-# wb_save2.save("files/ddexp.xlsx")
+wb_save2.save("files/6x.xlsx")
 
-# plt.plot(x, y, label="exp(x)")
-# plt.plot(x, yprime, label="dexp(x)")
-# plt.plot(x, ydoubleprime, label="ddexp(x)")
-# plt.legend()
-# plt.title("exp(x) & dexp(x) & ddexp(x)")
-# plt.xlabel("x")
-# plt.ylabel("y")
-# plt.show()
+
+plt.plot(x, y, label="x^3")
+plt.plot(x, yprime, label="3x^2")
+plt.plot(x, ydoubleprime, label="6x")
+plt.legend()
+plt.title("x^3 , 3x^2 , 6x")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
