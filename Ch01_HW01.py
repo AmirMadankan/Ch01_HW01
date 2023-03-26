5c6c2a31383a484aae22aeffcde662cb01ec6436
Amir Madankan <madankan.amir.2000@gmail.com>
2023-03-27 00:22:00 +0430

    the excel file exchanged and code updated

--- Ch01_HW01.py
+++ Ch01_HW01.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from openpyxl import load_workbook, Workbook
 
-ws = load_workbook("files/exp.xlsx")["Sheet1"]
+ws = load_workbook("files/x2.xlsx")["Sheet1"]
 max_row = ws.max_row
 
 data = []
@@ -16,7 +16,38 @@ y = data[:, 1]
 dy = y[1:] - y[:-1]
 dx = x[1:] - x[:-1]
 
+ddy = dy[1:] - dy[:-1]
+ddx = dx[1:] - dx[:-1]
+
 yprime = y.copy()
 yprime[1:] = dy/dx
 yprime[0] = yprime[1]
 
+ydoubleprime = yprime.copy()
+ydoubleprime[2:] = ddy/ddx
+# ydoubleprime[0] = ydoubleprime[1]
+
+# wb_save = Workbook()
+# ws_save = wb_save.active
+# wb_save2 = Workbook()
+# wb_save2 = wb_save2.active
+
+# i = 0
+# for x_i in x:
+#     ws_save[f"A{i+1}"] = x_i
+#     ws_save[f"B{i+1}"] = yprime[i]
+#     wb_save2[f"A{i+1}"] = x_i
+#     wb_save2[f"B{i+1}"] = ydoubleprime[i]
+#     i += 1
+
+# wb_save.save("files/dexp.xlsx")
+# wb_save2.save("files/ddexp.xlsx")
+
+# plt.plot(x, y, label="exp(x)")
+# plt.plot(x, yprime, label="dexp(x)")
+# plt.plot(x, ydoubleprime, label="ddexp(x)")
+# plt.legend()
+# plt.title("exp(x) & dexp(x) & ddexp(x)")
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.show()
