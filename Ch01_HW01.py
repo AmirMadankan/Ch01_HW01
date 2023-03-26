2e735f84c16c5ee6fe78065c023d9c86627bcfc9
Amir Madankan <madankan.amir.2000@gmail.com>
2023-03-26 20:30:17 +0430

    data defined

--- Ch01_HW01.py
+++ Ch01_HW01.py
@@ -4,3 +4,12 @@ from openpyxl import load_workbook, Workbook
 
 ws = load_workbook("files/exp.xlsx")["Sheet1"]
 max_row = ws.max_row
+
+data = []
+for i in range(1, max_row+1):
+    data.append([ws[f"A{i}"].value, ws[f"B{i}"].value])
+
+data = np.array(data)
+x = data[:, 0]
+y = data[:, 1]
+
